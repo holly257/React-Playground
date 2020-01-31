e695f5435167c4075696dacded9521dde47a7ea3
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-01-30 21:17:11 -0500

    added composition files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Split from './composition/Split';
+import './App.css';
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <Split>
+        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+      </Split>
+      <Split>
+        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
     </main>
   );
 }
---
+++ src/composition/Split.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Split.css';
+
+function Split(props){
+    return (
+        <div className="split" >
+            {props.children}        
+        </div>
+    );
+}
+
+export default Split;
