399611d9c109e7f72de0e73e9c8850175ccc13ef
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-02-18 13:56:45 -0500

    finished React Router section

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
 import React from 'react';
 import './App.css';
-import Split from './composition/Split';
-import Tooltip from './composition/Tooltip';
+// import Split from './composition/Split';
+// import Tooltip from './composition/Tooltip';
 // import TheDate from './state/TheDate';
 // import Counter from './state/Counter';
 // import Tabs from './Tabs';
 // import Accordion from './state-drills/Accordion';
 
+
+
 // make 2 tooltips here and another inside the App directly
-const firstTooltip = (
-  <Tooltip color='hotpink' message='tooltip message'>
-    ipsum
-  </Tooltip>
-)
-const secondTooltip = (
-  <Tooltip color='#126BCC' message='another tooltip message'>
-    officiis
-  </Tooltip>
-)
+// const firstTooltip = (
+//   <Tooltip color='hotpink' message='tooltip message'>
+//     ipsum
+//   </Tooltip>
+// )
+// const secondTooltip = (
+//   <Tooltip color='#126BCC' message='another tooltip message'>
+//     officiis
+//   </Tooltip>
+// )
 
-function App() {
-  return (
-    <main className='App'>
-      <hr />
-      <Split className='left' flexBasis='2'>
-        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
-        {/* make another tooltip directly inside the App */}
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
-      <Split className='right'>
-        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-    </main>
-  )
-}
+// function App() {
+//   return (
+//     <main className='App'>
+//       <hr />
+//       <Split className='left' flexBasis='2'>
+//         This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+//         {/* make another tooltip directly inside the App */}
+//         <Tooltip message='one more tooltip message'>
+//           Necessitatibus?
+//         </Tooltip>
+//       </Split>
+//       <Split className='right'>
+//         This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+//       </Split>
+//     </main>
+//   )
+// }
 
-export default App;
+// export default App;
 
 // const tabsProp = [
 //   { name: 'First tab',
