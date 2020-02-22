71d64721b3d6c332bbb355da7582dd711146111e
Holly Rogers <hollyrogers@Hollys-Air.hsd1.tn.comcast.net>
2020-02-22 12:22:21 -0600

    added error handling

--- src/demonym-app/demonymApp.js
+++ src/demonym-app/demonymApp.js
@@ -20,26 +20,44 @@ class DemonymApp extends React.Component {
 
     componentDidMount(){
         fetch('https://country.register.gov.uk/records.json?page-size=5000')
+            .then(response => {
+                console.log('about to check for errors');
+                if(!response.ok) {
+                    console.log('an error occured. let\'s throw an error');
+                    throw new Error('something went wrong');
+                } return response;
+            })
             .then(response => response.json())
             .then(data => {
                 const countries = Object.keys(data)
                     .map(key => data[key].item[0]);
                 this.setState({
-                    countries
+                    countries,
+                    error: null
+                });
+            })
+            .catch(err => {
+                this.setState({
+                    error: err.message
                 });
             });
     }
 
     render(){
+        const error = this.state.error
+            ? <div className='demonym_app__error'>{this.state.error}</div>
+            : '';
+
         const demon = this.state.selected
             ? <Demonym 
                 name={this.state.selected['citizen-names']}
                 country={this.state.selected.name} 
             />
-            : <div className='demonym_app__placeholder'>Select a country above</div>;
-
+            : <div className='demonym_app_placeholder'>Select a country above</div>  
+        
         return (
             <div className='demonym_app'>
+                {error}
                 <CountrySelector 
                     countries={this.state.countries} 
                     changeHandler={selected => this.setSelected(selected)}
