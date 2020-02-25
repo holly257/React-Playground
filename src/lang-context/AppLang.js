34b24577cda800118382e600dedcf98f1a901ef1
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-02-25 17:09:59 -0500

    added lang control buttons

--- src/lang-context/AppLang.js
+++ src/lang-context/AppLang.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Child from './Child/Child';
+import LangControls from './LangControls/LangControls';
 
 export default class AppLang extends Component {
     render() {
         return (
             <div className='AppLang'>
+                <br />
+                <LangControls />
                 <Child />
             </div>
         )
