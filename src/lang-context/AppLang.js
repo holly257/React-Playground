8bd36a01f7cad4cf3ec66ed4decd24d70b0a5ab9
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-02-27 15:20:03 -0500

    added button functionality

--- src/lang-context/AppLang.js
+++ src/lang-context/AppLang.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import Child from './Child/Child';
 import LangControls from './LangControls/LangControls';
+import LanguageContext from './languageContext';
 
 export default class AppLang extends Component {
+    state = {
+        lang: window.navigator.language 
+    }
+
+    handleSetLang = (lang) => {
+        this.setState({ lang })
+    };
+
     render() {
+        const contextValue = {
+            lang: this.state.lang
+        }
         return (
-            <div className='AppLang'>
-                <br />
-                <LangControls />
-                <Child />
-            </div>
+            <LanguageContext.Provider
+                value={contextValue}
+            >
+                <div className='AppLang'>
+                    <br />
+                    <LangControls 
+                        onSetLang={this.handleSetLang}
+                    />
+                    <Child />
+                </div>
+            </LanguageContext.Provider>
+            
         )
     }
 }
